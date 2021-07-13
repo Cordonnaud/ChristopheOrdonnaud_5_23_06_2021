8455d65f93a67c15d589f6d02273567a35390347
Ordonnaud <christophe.ordonnaud@expleogroup.com>
2021-07-13 15:47:45 +0200

    Av-2

--- js/Array.js
+++ js/Array.js
@@ -1,8 +1,1 @@
-// const newsIngredients = recipes.filter((ingredients => ingredients.name)
-// );
-
-// console.log(newIngredients);
-
-const found = recipes.find(Array => name = ingredients);
-
-console.log(found);
+// Avancement
